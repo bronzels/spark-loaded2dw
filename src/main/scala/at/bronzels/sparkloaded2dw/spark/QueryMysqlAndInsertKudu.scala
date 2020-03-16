0a7ba4623b04f662a8d95f317071f63f6d4fdef1
simo <hunanjsd@gmail.com>
2020-03-16 14:12:19 +0800

    1. 修复 mysql date 类型转换异常

--- src/main/scala/at/bronzels/sparkloaded2dw/spark/QueryMysqlAndInsertKudu.scala
+++ src/main/scala/at/bronzels/sparkloaded2dw/spark/QueryMysqlAndInsertKudu.scala
@@ -15,13 +15,13 @@ object QueryMysqlAndInsertKudu {
     val options = myCli.buildOptions()
     if (myCli.parseIsHelp(options, args)) return
 
-    val ss: SparkSession = MySparkUtil.getLocalSparkSession(args)
+    val ss: SparkSession = MySparkUtil.getSparkSession(args)
     val isSrcFieldNameWTUpperCase: Boolean = myCli.isSrcFieldNameWTUpperCase
     val fromHive: Boolean = myCli.sourceFromHive
     val kuduMaster = myCli.kuduConnUrl
     val table2KuduArr = myCli.tables2load
     val outputPrefix = myCli.outputPrefix
-    val mysqlUrl = "jdbc:mysql://%s/%s?user=%s&password=%s&zeroDateTimeBehavior=convert_to_null".format(myCli.dbConnUrl, myCli.dbName, myCli.dbUser, myCli.dbPassword)
+    val mysqlUrl = "jdbc:mysql://%s/%s?user=%s&password=%s&zeroDateTimeBehavior=convertToNull".format(myCli.dbConnUrl, myCli.dbName, myCli.dbUser, myCli.dbPassword)
     val mySQLEnvConf: MySQLEnvConf = new MySQLEnvConf(mysqlUrl, myCli.dbUser, myCli.dbPassword, myCli.dbName)
 
     /* (1, 2) 中 1 表示新加的 column 的 StructField, 2 表示 default 值, 用字符串表示，因为会用 cast 转换为 1 中指定的 dataType */
@@ -33,29 +33,30 @@ object QueryMysqlAndInsertKudu {
       //val kuduOutputTable = outputPrefix + myCli.dbName + "_" + tableName.toLowerCase
       val kuduOutputTable = outputPrefix + tableName.toLowerCase
       val hiveOutputTable = kuduOutputTable.replace("::", "_").replace(".", "_")
-      var afterAddFixColumnsDF: DataFrame = null
+      var sourceTableDF: DataFrame = null
       val tablePKs = MysqlDao.getMysqlTablePKFields(ss, mySQLEnvConf, tableName)
       tablePKs.foreach(u => {
-        println("table: %s, pk: %s".format(tableName, u))
+        println("table %s to load, table pks: %s".format(tableName, u))
       })
-      if(fromHive){
-        afterAddFixColumnsDF = ss.read.table(hiveOutputTable)
-        val dfSchema = afterAddFixColumnsDF.schema
-        tablePKs.foreach(field => {
-          if (!dfSchema.names.contains(field))
-            afterAddFixColumnsDF = afterAddFixColumnsDF.withColumnRenamed(field.toLowerCase, field)
-        })
-        afterAddFixColumnsDF = MysqlDao.convertMysqlDataType2DebeziumDataType(ss, afterAddFixColumnsDF, mySQLEnvConf, tableName)
-        afterAddFixColumnsDF = CommonStat.addColumnWithDefaultValue(afterAddFixColumnsDF, fixColumn2Add)
-      }else{
-        val rawMysqlDF = MysqlDao.getMysqlHugeTblDF(ss, mySQLEnvConf, tableName, tablePKs, 60)
-        afterAddFixColumnsDF = CommonStat.addColumnWithDefaultValue(rawMysqlDF, fixColumn2Add)
-      }
 
-      afterAddFixColumnsDF.schema.foreach( u => {
-        println("field:%s, type:%s".format(u.name, u.dataType.typeName))
+      if(fromHive)
+        sourceTableDF = ss.read.table(hiveOutputTable)
+      else
+        sourceTableDF = MysqlDao.getMysqlHugeTblDF(ss, mySQLEnvConf, tableName, tablePKs, 60)
+
+      var formatDataTypeDF = sourceTableDF
+      val dfSchema = sourceTableDF.schema
+      tablePKs.foreach(field => {
+        if (!dfSchema.names.contains(field))
+          formatDataTypeDF = formatDataTypeDF.withColumnRenamed(field.toLowerCase, field)
       })
-      KuDuDao.saveFormatDF2Kudu(ss, kuduMaster, afterAddFixColumnsDF, kuduOutputTable, tablePKs, isSrcFieldNameWTUpperCase)
+      formatDataTypeDF = MysqlDao.convertMysqlDataType2DebeziumDataType(ss, formatDataTypeDF, mySQLEnvConf, tableName)
+      formatDataTypeDF = CommonStat.addColumnWithDefaultValue(formatDataTypeDF, fixColumn2Add)
+
+      formatDataTypeDF.schema.foreach( u => {
+        println("table: %s, field:%s, type:%s".format(tableName, u.name, u.dataType.typeName))
+      })
+      KuDuDao.saveFormatDF2Kudu(ss, kuduMaster, formatDataTypeDF, kuduOutputTable, tablePKs, isSrcFieldNameWTUpperCase)
     })
   }
 
